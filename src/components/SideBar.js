920eb5d0ee9260b32be29ba256b4ff870243ea98
lupacco <pagotolc@gmail.com>
2022-11-21 17:52:09 -0300

    Alterando nome e imagem de usuário OK

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,16 +1,29 @@
+import React from "react"
 import Sugestoes from "./Sugestoes"
 import catanacomics from "../assets/img/catanacomics.svg"
 
 export default function SideBar(props){
+    const [userName, setUserName] = React.useState(props.userName)
+    const [img, setImg] = React.useState(props.userImg)
+
     return (
         <div class="sidebar">
         <div data-test="user" class="usuario">
-          <img data-test="profile-image" src={props.userImg} />
+          <img alt="" data-test="profile-image" src={img} onClick={() => {
+            let newImg = prompt("Nova url para imagem de perfil do usuário!")
+            if(!newImg){
+              alert('Insira uma url')
+            }else{
+              setImg(newImg)
+            }
+          }}/>
           <div class="texto">
             <strong>{props.userNick}</strong>
             <span data-test="name">
-              {props.userName}
-              <ion-icon data-test="edit-name" name="pencil"></ion-icon>
+              {userName}
+              <ion-icon onClick={() => {
+                setUserName(prompt("Novo nome de usuário!"))
+              }} data-test="edit-name" name="pencil"></ion-icon>
             </span>
           </div>
         </div>
